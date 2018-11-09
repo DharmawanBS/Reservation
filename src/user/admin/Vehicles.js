481803d7e0cc4a7ff6eb1670096c7ce92f468d16
Fintanto Cendikia <fincendi@gmail.com>
2018-11-09 09:53:29 +0700

    <bugged>

--- src/user/admin/Vehicles.js
+++ src/user/admin/Vehicles.js
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
-import { Grid, Paper, Typography, ExpansionPanel, ExpansionPanelSummary, ExpansionPanelDetails, Button, GridList, GridListTile, GridListTileBar } from "@material-ui/core";
+import { Grid, Paper, Typography, ExpansionPanel, ExpansionPanelSummary, ExpansionPanelDetails, Button, GridList, GridListTile, GridListTileBar, IconButton, TextField } from "@material-ui/core";
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import AddIcon from '@material-ui/icons/Add';
+import DeleteIcon from '@material-ui/icons/DeleteOutline';
+import EditIcon from '@material-ui/icons/Edit';
+import DoneIcon from '@material-ui/icons/Done';
 
 const tileData = [
     {
@@ -58,7 +61,19 @@ const tileData = [
 
 class Vehicle extends Component {
   state = {
-      expanded : null
+      expanded : null,
+      edit : false,
+      spec : [
+        "- Jumlah kursi 59 seat",
+        "- Air Conditioner <br />",
+        "- Mic <br />",
+        "- TV <br />",
+        "- DVD <br />",
+        "- Karaoke <br />",
+        "- cool box <br />",
+        "- crew yg berpengalaman <br />",
+        "- helper yg ramah <br />"
+      ]
   };
 
   handleChange = panel => (event, expanded) => {
@@ -77,13 +92,24 @@ class Vehicle extends Component {
             <Typography variant='h5'>Big Bus T 35 T</Typography>
           </ExpansionPanelSummary>
           <ExpansionPanelDetails>
-          <Grid style={{flexDirection:'row', paddingTop:16}} container>
-                <GridList style={{transform:'translateZ(0)', flex:1, justifyContent:'center'}}>
+          <Grid style={{paddingTop:16}}>
+              <Grid container>
+                <GridList style={{transform:'translateZ(0)', flex:1, justifyContent:'center', overflow:'auto', maxHeight:500}}>
                     {tileData.map(tile => (
                     <GridListTile key={tile.img} style={{height:100, width:200}}>
                         <img src={tile.img} alt={tile.title}/>
                         <GridListTileBar
                         title={tile.title}
+                        actionIcon={
+                          this.state.edit?
+                          (
+                            <IconButton>
+                              <DeleteIcon style={{color:'#FFF'}}/>
+                            </IconButton>
+                          )
+                          :
+                          null
+                        }
                         />
                     </GridListTile>
                     ))}
@@ -92,17 +118,37 @@ class Vehicle extends Component {
                 <Typography variant="h5" id="simple-modal-description" style={{textAlign:'left', flex:0.5, marginLeft:24}}>
                     Spesifikasi
                 </Typography>
-                <Typography variant='subheading' id="simple-modal-description" style={{textAlign:'left', flex:0.5, marginLeft:8,padding:16, fontWeight:'normal'}}>
-                    - Jumlah kursi 59 seat <br />
-                    - Air Conditioner <br />
-                    - Mic <br />
-                    - TV <br />
-                    - DVD <br />
-                    - Karaoke <br />
-                    - cool box <br />
-                    - crew yg berpengalaman <br />
-                    - helper yg ramah <br />
-                </Typography>
+                <Grid style={{overflow:'auto', maxHeight:500}}>
+                {
+                  !this.state.edit?
+                    <Typography variant='subheading' id="simple-modal-description" style={{textAlign:'left', flex:0.5, marginLeft:8,padding:16, fontWeight:'normal'}}>
+                      - Jumlah kursi 59 seat <br />
+                      - Air Conditioner <br />
+                      - Mic <br />
+                      - TV <br />
+                      - DVD <br />
+                      - Karaoke <br />
+                      - cool box <br />
+                      - crew yg berpengalaman <br />
+                      - helper yg ramah <br />
+                  </Typography>
+                  :
+                  this.state.spec.map((item)=>(
+                    <TextField
+                      id="outlined-name"
+                      value={item}
+                      onChange={this.handleChange('name')}
+                      margin="normal"
+                      variant="outlined"
+                      style={{width:'100%'}}
+                      onSubmit={()=>{
+                        this.state.spec.push("")
+                          this.setState({spec:this.state.spec})
+                      }}
+                    />
+                  ))
+                }
+                </Grid>
                 <Typography variant="h5" id="simple-modal-description" style={{textAlign:'left', flex:0.5, marginLeft:24}}>
                     Harga
                 </Typography>
@@ -110,6 +156,21 @@ class Vehicle extends Component {
                     Rp. xxx.xxx.xxx / day
                 </Typography>
                 </Grid>
+              </Grid>
+              <Grid style={{textAlign:'right'}}>
+                {
+                  this.state.edit?
+                  <Button variant='outlined' color='primary' onClick={()=>this.setState({edit:false})}>
+                    SAVE
+                    <DoneIcon style={{fontSize:15, marginLeft:8}}/>
+                  </Button>
+                  :
+                  <Button variant='outlined' onClick={()=>this.setState({edit:true})}>
+                    EDIT
+                    <EditIcon style={{fontSize:15, marginLeft:8}}/>
+                  </Button>
+                }
+              </Grid>
             </Grid>
           </ExpansionPanelDetails>
         </ExpansionPanel>
@@ -126,7 +187,7 @@ class Vehicle extends Component {
           </ExpansionPanelDetails>
         </ExpansionPanel>
         </Paper>
-        <Button variant="fab" color="primary" aria-label="Add" style={{position:'absolute', right:'5%', bottom:'5%'}}>
+        <Button variant="fab" color="primary" aria-label="Add" style={{position:'fixed', right:'5%', bottom:'5%'}}>
             <AddIcon />
         </Button>
     </Grid>
